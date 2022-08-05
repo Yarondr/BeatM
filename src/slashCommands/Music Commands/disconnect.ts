5808fd4e76c3c3de286eac9c3659158a73394188
Yarondr <yarondrukh@gmail.com>
2022-08-05 23:12:13 +0300

    Create disconnect and join commands

---
+++ src/slashCommands/Music Commands/disconnect.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, GuildMember } from "discord.js";
+import { getMember } from "../../utils/djs";
+import { IBot } from "../../utils/interfaces/IBot";
+import { ISlashCommand } from "../../utils/interfaces/ISlashCommand";
+
+module.exports = {
+    name: "disconnect",
+    category: "Music Commands",
+    description: "Disconnect from the voice channel and clears the queue.",
+    botPermissions: ['SendMessages', 'EmbedLinks'],
+    
+    execute: async (bot: IBot, interaction: CommandInteraction) => {
+        if (!interaction.isChatInputCommand()) return;
+        const guild = bot.client.guilds.cache.get(interaction.guildId!)!;
+        const member: GuildMember = await getMember(guild, interaction.member?.user.id!);
+        const queue = bot.player.getQueue(interaction.guildId!);
+        
+        if (!member.voice.channel) {
+            return interaction.reply("You must be in a voice channel to play music.");
+        }
+        if (!queue || !queue.connection) {
+            return interaction.reply("I'm not in a voice channel!");
+        }
+        if (member.voice.channel.id != queue.connection.channel.id) {
+            return interaction.reply("You must be in the same voice channel as the bot to use this command.");
+        }
+        queue.destroy()
+        await interaction.reply(`Disconnected from \`${member.voice.channel.name}\``);
+    }
+} as ISlashCommand
---
+++ src/slashCommands/Music Commands/join.ts
@@ -0,0 +1,42 @@
+import { CommandInteraction, GuildMember, TextChannel } from "discord.js";
+import { getMember } from "../../utils/djs";
+import { IBot } from "../../utils/interfaces/IBot";
+import { ISlashCommand } from "../../utils/interfaces/ISlashCommand";
+
+module.exports = {
+    name: "join",
+    category: "Music Commands",
+    description: "Join to the user's voice channel.",
+    botPermissions: ['SendMessages', 'EmbedLinks', 'Connect', 'Speak'],
+    
+    execute: async (bot: IBot, interaction: CommandInteraction) => {
+        if (!interaction.isChatInputCommand()) return;
+
+        const guild = bot.client.guilds.cache.get(interaction.guildId!)!;
+        const member: GuildMember = await getMember(guild, interaction.member?.user.id!);
+        const channel = guild?.channels.cache.get(interaction.channelId!)! as TextChannel;
+        const player = bot.player;
+
+
+        if (!member.voice.channel) {
+            return interaction.reply("You must be in a voice channel to play music.");
+        }
+        await interaction.deferReply();
+
+        // create queue and join voice channel
+        const queue = player.createQueue(guild, {
+            metadata: channel,
+        });
+        try {
+            if (!queue.connection) {
+                await queue.connect(member.voice.channel);
+                await interaction.editReply(`Joined \`${member.voice.channel.name}\` And bound to <#${channel.id}>`);
+            } else {
+                await interaction.editReply(`Can't join to \`${member.voice.channel.name}\` because I'm already in a voice channel.`);
+            }
+        } catch {
+            player.deleteQueue(guild.id);
+            return interaction.editReply(`I can't join your voice channel. ${member.id}`);
+        }
+    }
+} as ISlashCommand;
